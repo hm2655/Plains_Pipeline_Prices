9ba7066a0f342c5d1225993fa4ad969beca08b8f
Harshit <hm2655@columbia.edu>
2017-06-19 17:38:20 -0500

    Code to scrap
    This code is to scrap all the links related to crude oil price bulletin on Plains Pipeline. This uses Woodmac Houston's proxy.

---
+++ Plains_Price_Scrapping.R
@@ -0,0 +1,26 @@
+library(httr)
+library(rvest)
+proxy.string<-use_proxy("http://prox-hou-pri.woodmac.com", port=8080)
+url<-"https://www.plainsallamerican.com/customer-center/crude-oil-bulletins"
+session<-html_session(url, proxy.string)
+link_list<-read_html(session) %>% html_nodes("a") %>% html_attr("href")
+a<-matrix(nrow = length(link_list), ncol=1)
+for(i in 1:length(link_list))
+{
+  if(!grepl(".pdf", link_list[i]))
+  {
+    a[i]<-"true"
+  } else 
+  {
+    a[i]<-link_list[i]
+  }
+}
+
+a<-a[a[,1]!="true"]
+
+nrow(a)for(i in 1:length(a))
+{
+  link<-paste("https://www.plainsallamerican.com",a[1], sep="")
+  dest<-paste(i,".pdf", sep="")
+  download.file(url, destfile = dest, mode = "wb")
+}
